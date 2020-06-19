aa741487d73d8304d4ebc17e167831962601c181
Takayuki Maeda <41065217+TaKO8Ki@users.noreply.github.com>
2020-06-20 01:08:50 +0900

    add abc114

---
+++ abc114/src/bin/c.rs
@@ -0,0 +1,21 @@
+use proconio::{fastout, input};
+
+#[fastout]
+fn main() {
+    input!(n: usize);
+    let mut count: usize = 0;
+    dfs(n, 0, &mut count, 0, 0, 0);
+    println!("{}", count);
+}
+
+fn dfs(n: usize, a: usize, count: &mut usize, x: usize, y: usize, z: usize) {
+    if a <= n && x >= 1 && y >= 1 && z >= 1 {
+        *count += 1
+    }
+    if a >= n {
+        return;
+    }
+    dfs(n, a * 10 + 3, count, x + 1, y, z);
+    dfs(n, a * 10 + 5, count, x, y + 1, z);
+    dfs(n, a * 10 + 7, count, x, y, z + 1);
+}
